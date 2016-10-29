1ec7bce75840c37e5fe0e83163e999ecdbeb1c2e
shekodn <sdiazn19@gmail.com>
2016-10-29 12:09:15 -0500

    sniffs arp traffic

---
+++ test.py
@@ -0,0 +1,17 @@
+## Import Scapy module
+from scapy.all import *
+
+file = open("arp.txt", "w")
+
+def arp_traffic(packet):
+    if packet[ARP].op == 1: #who-has (request)
+        file.write("Request: " + packet[ARP].psrc + " is asking about " + packet[ARP].pdst + '\n')
+        return "Request: " + packet[ARP].psrc + " is asking about " + packet[ARP].pdst
+
+
+    if packet[ARP].op == 2: #is-at (response)
+        file.write("*Response: " + packet[ARP].hwsrc + " has address " + packet[ARP].psrc + '\n')
+        return "*Response: " + packet[ARP].hwsrc + " has address " + packet[ARP].psrc
+
+
+print sniff(prn=arp_traffic, filter="arp", store=0, count=10)
