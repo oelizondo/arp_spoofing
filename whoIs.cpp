c901fe6f195e54502b24cf9bb762d1411f358d68
oelizondo <oscarmarcelo95@gmail.com>
2016-11-04 14:55:12 -0600

    refactors ifspoofing function to not go over threshold

--- whoIs.cpp
+++ whoIs.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
+#include <map>
 
 using namespace std;
-
 
 struct arpPacket{
   string option;
@@ -11,11 +12,29 @@ struct arpPacket{
   string sIp;
 };
 
+const int THRESHOLD = 10;
 
+int optionDirfference(vector<arpPacket> packets) {
+  int option1, option2 = 0;
+
+  for (int i = 0; i < packets.size(); i++) {
+    if (packets[i].option == "2")
+      option2++;
+    if(packets[i].option == "1")
+      option1++;
+  }
+  return option2 - option1;
+}
+
+bool isSpoofing (vector<arpPacket> packets) {
+  return optionDirfference(packets) >= THRESHOLD;
+}
+
+// Write packets to file
 vector<arpPacket> writePacketstoFile() {
   vector<arpPacket> packets;
   ifstream myFile;
-
+  map<string, string> ARPtable;
 
   myFile.open("arp.txt");
   string sOption;
@@ -31,27 +50,11 @@ vector<arpPacket> writePacketstoFile() {
   }
 
   myFile.close();
-
   return packets;
-}
-
-bool isSpoofing (vector<arpPacket> packets,string userIP,string userMAC) {
-
-  while (!packets.empty()) {
-    if (packets.back().sIp == userIP && packets.back().sMac != userMAC) {
-
-      //Regresa la MAC del atacante//
-      cout<<packets.back().sMac;
-      return true;
-    }
-    packets.pop_back();
-  }
-  return false;
 }
 
 int main() {
   vector<arpPacket> packets;
   packets = writePacketstoFile();
-
-  isSpoofing(packets,"10.189.15.142","e1:c1:81:01:f1:c1");
+  isSpoofing(packets);
 }
