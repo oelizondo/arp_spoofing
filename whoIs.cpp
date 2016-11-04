6d3b3e5e4f3cf5226546c3844838e3f3f9dbb279
shekodn <sdiazn19@gmail.com>
2016-11-04 17:17:48 -0600

    adds ip's and mac of attacker

--- whoIs.cpp
+++ whoIs.cpp
@@ -13,19 +13,64 @@ struct arpPacket{
 };
 
 const int THRESHOLD = 5;
-map<string, int> arpMapper;
-void findSpoofer() {
-  map<string, int>::iterator it = arpMapper.begin();
+map<string, int> macMapper;
+map<string, int> ipMapper;
 
-  for (it = arpMapper.begin(); it != arpMapper.end(); it++) {
-    if (it->second > THRESHOLD) {
-      cout << "Mac of spoofer: " << it->first << endl;
+arpPacket atacante;
+arpPacket victima;
+
+
+
+string findMacOfSpoofer() {
+    string macOfSpoofer = "no spoofing";
+    map<string, int>::iterator it = macMapper.begin();
+    for (it = macMapper.begin(); it != macMapper.end(); it++) {
+        if (it->second > THRESHOLD) {
+            //cout << "Mac of spoofer: " << it->first << endl;
+              return it->first;
+        }
     }
+    return macOfSpoofer;
+}
+
+
+void findIPs(string sMacOfSpoofer, vector<arpPacket> packets) {
+  map<string, int>::iterator it = ipMapper.begin();
+  int iCont = 0;
+  string matiPs[2][2];
+
+  //meter al mapa
+  for(int i = 0; i < packets.size(); i++){
+      if(packets[i].sMac == sMacOfSpoofer){
+          ipMapper[packets[i].sIp]++;
+      }
+  }
+
+  for (it = ipMapper.begin(); it != ipMapper.end(); it++) {
+      //cout << it->first << "->" << it->second << endl;
+      matiPs[iCont][0] = it->first;
+      matiPs[iCont][1] = it->second;
+      iCont++;
+
+  }
+
+  if(matiPs[0][1] > matiPs[1][1] ){
+      //cout << "IP of victim " << matiPs[0][0] << endl;
+      victima.sIp = matiPs[0][0];
+      //cout << "IP of attacker " << matiPs[1][0] << endl;
+      atacante.sIp = matiPs[1][0];
+
+
+  } else{
+      //cout << "IP of victim " << matiPs[1][0] << endl;
+      victima.sIp = matiPs[1][0];
+      //cout << "IP of attacker " << matiPs[0][0] << endl;
+      atacante.sIp = matiPs[0][0];
   }
 }
 
 void insertIntoMap(string mac) {
-  arpMapper[mac] = 0;
+  macMapper[mac] = 0;
 }
 
 int optionDirfference(vector<arpPacket> packets) {
@@ -34,11 +79,11 @@ int optionDirfference(vector<arpPacket> packets) {
   for (int i = 0; i < packets.size(); i++) {
     if (packets[i].option == "2") {
       option++;
-      arpMapper[packets[i].sMac]++;
+      macMapper[packets[i].sMac]++;
     }
     if (packets[i].option == "1") {
       option--;
-      arpMapper[packets[i].sMac]--;
+      macMapper[packets[i].sMac]--;
     }
   }
 
@@ -72,9 +117,22 @@ vector<arpPacket> writePacketstoFile() {
   return packets;
 }
 
+void printAnswer(arpPacket victima, arpPacket atacante){
+
+    cout << "  Mac of victim:  " << victima.sMac << endl;
+    cout << "Mac of attacker:  " << atacante.sMac << endl;
+    cout << "   IP of victim:  " << victima.sIp << endl;
+    cout << " IP of attacker:  " << atacante.sIp << endl;
+}
+
 int main() {
   vector<arpPacket> packets;
   packets = writePacketstoFile();
   isSpoofing(packets);
-  findSpoofer();
+
+  atacante.sMac = findMacOfSpoofer();
+  findIPs(atacante.sMac, packets);
+
+  printAnswer(victima, atacante);
+
 }
